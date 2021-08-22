3b9048cdcd452a67492455c0208f973ce4111cd4
Jakub Lasek <jakublasek47@gmail.com>
2021-08-22 18:38:58 +0200

    Initial commit + add basic project directories structure

---
+++ projectx/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        In memory of Billy Harrington
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ projectx/src/components/layout/PageNav/PageNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const PageNav = () => (
+    <nav>
+        <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Game</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/gamepotions`} activeClassName='active'>Options</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/stockmarket`} activeClassName='active'>Market</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/stores`} activeClassName='active'>Stores</NavLink>
+    </nav>
+);
+
+export default PageNav;
