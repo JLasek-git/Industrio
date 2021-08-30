d9fe86d576fe32c7f10c74e26c9c442ddec3a4e6
Jakub Lasek <jakublasek47@gmail.com>
2021-08-30 16:19:49 +0200

    Add prevent buying when there is no proper amount of money on player account. Add styles for machines list in store

--- projectx/src/components/views/Stores/Store1/Store1.js
+++ projectx/src/components/views/Stores/Store1/Store1.js
@@ -5,41 +5,48 @@ import Section from "../../../layout/Section/Section";
 import GameWindow from "../../../layout/GameWindow/GameWindow";
 import ChangeShopButton from "../../../common/ChangeShopButton/ChangeShopButton";
 import Button from "../../../common/Button/Button";
-
-
+import machineImg from "../../../../images/inpact_hitter_1.png";
 /* Machines store */
 
-function Store1({...props}) {
-
+function Store1({ ...props }) {
   const handleBuy = () => {
-    const playerMachineQuantity = props.playerInfo.equipment.machines.impactCrusher.owned;
+    const playerMachineQuantity =
+      props.playerInfo.equipment.machines.impactCrusher.owned;
     const playerMoney = props.playerInfo.money;
     const machineCost = props.playerInfo.equipment.machines.impactCrusher.price;
     const increasedMachineQuantity = playerMachineQuantity + 1;
     const playerMoneyAfterBuy = playerMoney - machineCost;
 
-    props.setMachineEqQuantity(increasedMachineQuantity);
-    props.setMoney(playerMoneyAfterBuy);
+    if (playerMoneyAfterBuy >= 0) {
+      props.setMachineEqQuantity(increasedMachineQuantity);
+      props.setMoney(playerMoneyAfterBuy);
+    } else {
+      alert("You don't have enough money to buy this machine.");
+    }
   };
 
-  return(
+  return (
     <Section>
-    <GameWindow>
-      <ChangeShopButton>
-        <LinkButton
-          buttonText="Buildings"
-          iconName="shopping-basket"
-          link="/stores/store2"
-        />
-      </ChangeShopButton>
-      <div onClick={() => handleBuy()}>
-        <Button btnText="Impact Crusher" />
-      </div>
-      <p>Price: $10 000</p>
-    </GameWindow>
-  </Section>
-  )
+      <GameWindow>
+        <ChangeShopButton>
+          <LinkButton
+            buttonText="Buildings"
+            iconName="shopping-basket"
+            link="/stores/store2"
+          />
+        </ChangeShopButton>
+        <div className={styles.machinesList}>
+          <div className={styles.singleMachine}>
+            <img src={machineImg} alt="impact-hitter" />
+            <div onClick={() => handleBuy()}>
+              <Button btnText="Impact Crusher" />
+            </div>
+            <p>Price: $10 000</p>
+          </div>
+        </div>
+      </GameWindow>
+    </Section>
+  );
 }
-
 
 export default Store1;
