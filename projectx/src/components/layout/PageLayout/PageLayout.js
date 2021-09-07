9a2abda9dc742d66d6f6087067baa562f89affcc
Jakub Lasek <jakublasek41@gmail.com>
2021-09-07 16:37:42 +0200

    Add RWD for app

--- projectx/src/components/layout/PageLayout/PageLayout.js
+++ projectx/src/components/layout/PageLayout/PageLayout.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PageLayout.module.scss";
 import PageNav from "../PageNav/PageNav";
 import ProfilePanel from "../ProfilePanel/ProfilePanelContainer";
+import Icon from "../../common/Icon/Icon";
 
 function PageLayout() {
+  const [panels, isVisible] = useState(true);
+
+  const showPanels = () => {
+    isVisible(!panels);
+  };
+
   return (
     <div className={styles.pageLayoutContainer}>
       <div className={styles.windowBorder}>
@@ -12,10 +19,18 @@ function PageLayout() {
         <div className={styles.yellowCircle}></div>
         <div className={styles.greenCircle}></div>
       </div>
-      <div className={styles.leftPanels}>
-        <ProfilePanel />
-        <PageNav />
+      <div className={styles.hamburgerIconWrapper}>
+        <div className={styles.hamburgerIcon} onClick={showPanels}>
+          <Icon name="bars" />
+        </div>
       </div>
+
+      {panels && (
+        <div className={styles.leftPanels}>
+          <ProfilePanel closePanels={showPanels} />
+          <PageNav />
+        </div>
+      )}
     </div>
   );
 }
