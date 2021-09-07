aff5d6a0d88c6b53aa65774019faa2991f3f6b44
Jakub Lasek <jakublasek41@gmail.com>
2021-09-07 14:58:52 +0200

    Add option to sell machine in store machine

--- projectx/src/components/views/Stores/Store1/StoreMachine/StoreMachine.js
+++ projectx/src/components/views/Stores/Store1/StoreMachine/StoreMachine.js
@@ -18,6 +18,8 @@ function StoreMachine({
       props.playerInfo.magazine.poorMagazine.machinesCapacity,
     playerAllMachinesQuantity:
       props.playerInfo.equipment.machines.allMachinesQuantity,
+    playerMachineState:
+      props.playerInfo.equipment.machines[machineStateName].work,
   };
 
   const handleBuy = () => {
@@ -43,14 +45,43 @@ function StoreMachine({
     }
   };
 
+  const handleSell = () => {
+    const machineQuantity = reduxStateInfo.playerMachineQuantity - 1;
+    const playerAllMachinesQunatityOnSell =
+      reduxStateInfo.playerAllMachinesQuantity - 1;
+    const playerMoneyAfterSell = reduxStateInfo.playerMoney + machinePrice / 2;
+    if (machineQuantity >= 0) {
+      if (!reduxStateInfo.playerMachineState) {
+        props.setMachineEqQuantity({
+          machineQuantity,
+          machineStateName,
+        });
+        props.setMoney(playerMoneyAfterSell);
+        props.setAllMachinesQuantity(playerAllMachinesQunatityOnSell);
+      } else {
+        alert("You can't sell machine that currently works");
+      }
+    } else {
+      alert("You don't have more machines to sell.");
+    }
+  };
+
   return (
     <div className={styles.singleMachine}>
       <img src={machineImg} alt={machineStateName} />
       <p>{machineName}</p>
-      <div onClick={() => handleBuy()}>
-        <Button btnText="Buy" />
+      <div className={styles.actionHandlers}>
+        <div onClick={() => handleBuy()}>
+          <Button btnText="Purchase" />
+        </div>
+        <div onClick={() => handleSell()}>
+          <Button btnText="Sell" />
+        </div>
       </div>
-      <p>Price: ${machinePrice}</p>
+      <div className={styles.pricesContainer}>
+        <p>Purchase price: ${machinePrice}</p>
+        <p>Selling price: ${machinePrice / 2}</p>
+      </div>
     </div>
   );
 }
--- projectx/src/redux/store.js
+++ projectx/src/redux/store.js
@@ -8,18 +8,28 @@ import globalReducer from "./globalRedux";
 import playerReducer from "./playerRedux";
 import { saveState } from "./localStorage";
 
-const localStorageState = localStorage.state === undefined ? 'localStorageState' : JSON.parse(localStorage.state);
+const localStorageState =
+  localStorage.state === undefined
+    ? "localStorageState"
+    : JSON.parse(localStorage.state);
 
 /* local storage state settings */
-if(localStorageState !== "localStorageState"){
-  const machinesObjectInPlayerEq = localStorageState.playerInfo.equipment.machines;
-  for(let machine in machinesObjectInPlayerEq )
-  {
-    if(machine !== 'allMachinesQuantity'){
-      if(machinesObjectInPlayerEq[machine].materialFromProduction > 0){
-        localStorageState.playerInfo.equipment.materials.ironOre.quantity += machinesObjectInPlayerEq[machine].materialFromProduction;
-        localStorageState.playerInfo.money += machinesObjectInPlayerEq[machine].materialFromProduction * 10 * localStorageState.playerInfo.equipment.machines[machine].machineWorking;
-        localStorageState.playerInfo.equipment.machines[machine].machineWorking = 0;
+if (localStorageState !== "localStorageState") {
+  const machinesObjectInPlayerEq =
+    localStorageState.playerInfo.equipment.machines;
+  for (let machine in machinesObjectInPlayerEq) {
+    if (machine !== "allMachinesQuantity") {
+      if (machinesObjectInPlayerEq[machine].materialFromProduction > 0) {
+        localStorageState.playerInfo.equipment.materials.ironOre.quantity +=
+          machinesObjectInPlayerEq[machine].materialFromProduction;
+        localStorageState.playerInfo.money +=
+          machinesObjectInPlayerEq[machine].materialFromProduction *
+          10 *
+          localStorageState.playerInfo.equipment.machines[machine]
+            .machineWorking;
+        localStorageState.playerInfo.equipment.machines[
+          machine
+        ].machineWorking = 0;
       }
       machinesObjectInPlayerEq[machine].work = false;
       machinesObjectInPlayerEq[machine].timeDuration = 0;
@@ -28,168 +38,170 @@ if(localStorageState !== "localStorageState"){
   }
 }
 /* make initial state */
-const initialState = localStorage.state !== undefined ? localStorageState : {
-  playerInfo: {
-    avatar: profileIcon,
-    nickname: "Player 1",
-    experience: 0,
-    toNextLevel: 4000,
-    money: 50000,
-    equipment: {
-      machines: {
-        impactCrusher: {
-          id: "impact-crusher",
-          name: "Impact Crusher",
-          price: 10000,
-          requirement: 0,
-          type: "crusher",
-          application: "pre-treatment-of-ore",
-          performance: 15,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
-        jawCrusher: {
-          id: "jaw-crusher",
-          name: "Jaw Crusher",
-          price: 20000,
-          requirement: 10,
-          type: "crusher",
-          application: "pre-treatment-of-ore",
-          performance: 25,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
-        coneCrusher: {
-          id: "cone-crusher",
-          name: "Cone Crusher",
-          price: 35000,
-          requirement: 20,
-          type: "crusher",
-          application: "pre-treatment-of-ore",
-          performance: 50,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
-        hammerCrusher: {
-          id: "hammer-crusher",
-          name: "Hammer Crusher",
-          price: 50000,
-          requirement: 30,
-          type: "crusher",
-          application: "pre-treatment-of-ore",
-          performance: 80,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
-        ballDrumMill: {
-          id: "ball-drum-mill",
-          name: "Ball Drum Mill",
-          price: 100000,
-          requirement: 40,
-          type: "mill",
-          application: "pre-treatment-of-ore",
-          performance: 150,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
-        rodDrumMill: {
-          id: "rod-drum-mill",
-          name: "Rod Drum Mill",
-          price: 180000,
-          requirement: 70,
-          type: "mill",
-          application: "pre-treatment-of-ore",
-          performance: 250,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
-        drumScreen: {
-          id: "drum-screen",
-          name: "Drum Screen",
-          price: 250000,
-          requirement: 100,
-          type: "screens-and-separators",
-          application: "pre-treatment-of-ore",
-          performance: 750,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
+const initialState =
+  localStorage.state !== undefined
+    ? localStorageState
+    : {
+        playerInfo: {
+          avatar: profileIcon,
+          nickname: "Player 1",
+          experience: 0,
+          toNextLevel: 4000,
+          money: 50000,
+          equipment: {
+            machines: {
+              impactCrusher: {
+                id: "impact-crusher",
+                name: "Impact Crusher",
+                price: 10000,
+                requirement: 0,
+                type: "crusher",
+                application: "pre-treatment-of-ore",
+                performance: 15,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
+              jawCrusher: {
+                id: "jaw-crusher",
+                name: "Jaw Crusher",
+                price: 20000,
+                requirement: 10,
+                type: "crusher",
+                application: "pre-treatment-of-ore",
+                performance: 25,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
+              coneCrusher: {
+                id: "cone-crusher",
+                name: "Cone Crusher",
+                price: 35000,
+                requirement: 20,
+                type: "crusher",
+                application: "pre-treatment-of-ore",
+                performance: 50,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
+              hammerCrusher: {
+                id: "hammer-crusher",
+                name: "Hammer Crusher",
+                price: 50000,
+                requirement: 30,
+                type: "crusher",
+                application: "pre-treatment-of-ore",
+                performance: 80,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
+              ballDrumMill: {
+                id: "ball-drum-mill",
+                name: "Ball Drum Mill",
+                price: 100000,
+                requirement: 40,
+                type: "mill",
+                application: "pre-treatment-of-ore",
+                performance: 150,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
+              rodDrumMill: {
+                id: "rod-drum-mill",
+                name: "Rod Drum Mill",
+                price: 180000,
+                requirement: 70,
+                type: "mill",
+                application: "pre-treatment-of-ore",
+                performance: 250,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
+              drumScreen: {
+                id: "drum-screen",
+                name: "Drum Screen",
+                price: 250000,
+                requirement: 100,
+                type: "screens-and-separators",
+                application: "pre-treatment-of-ore",
+                performance: 750,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
 
-        magneticSeparator: {
-          id: "magneticSeparator",
-          name: "Magnetic separator",
-          price: 650000,
-          requirement: 150,
-          type: "screens-and-separators",
-          application: "pre-treatment-of-ore",
-          performance: 2000,
-          work: false,
-          machineWorking: 0,
-          timeDuration: 0,
-          materialFromProduction: 0,
-          owned: 0,
-        },
+              magneticSeparator: {
+                id: "magneticSeparator",
+                name: "Magnetic separator",
+                price: 650000,
+                requirement: 150,
+                type: "screens-and-separators",
+                application: "pre-treatment-of-ore",
+                performance: 2000,
+                work: false,
+                machineWorking: 0,
+                timeDuration: 0,
+                materialFromProduction: 0,
+                owned: 0,
+              },
 
-        allMachinesQuantity: 0,
-      },
+              allMachinesQuantity: 0,
+            },
 
-      materials: {
-        ironOre: {
-          id: "iron-ore",
-          name: "Iron ore",
-          price: 100,
-          durability: 300,
-          experience: 7.5,
-          productionCost: 10,
-          quantity: 100,
-        },
+            materials: {
+              ironOre: {
+                id: "iron-ore",
+                name: "Iron ore",
+                price: 100,
+                durability: 300,
+                experience: 7.5,
+                productionCost: 10,
+                quantity: 100,
+              },
 
-        ironOreConcentrate: {
-          id: "iron-ore-concentrate",
-          name: "Iron ore concentrate",
-          price: 200,
-          durability: 450,
-          experience: 7.5,
-          quantity: 0,
+              ironOreConcentrate: {
+                id: "iron-ore-concentrate",
+                name: "Iron ore concentrate",
+                price: 200,
+                durability: 450,
+                experience: 7.5,
+                quantity: 0,
+              },
+            },
+          },
+          magazine: {
+            poorMagazine: {
+              machinesCapacity: 6,
+            },
+          },
         },
-      },
-    },
-    magazine: {
-      poorMagazine: {
-        machinesCapacity: 6,
-      },
-    },
-  },
-  materialsInfo: materials,
-  machinesInfo: machinesPreTreatment,
-};
- 
+        materialsInfo: materials,
+        machinesInfo: machinesPreTreatment,
+      };
+
 /* define reducers */
 const reducers = {
   playerInfo: playerReducer,
 };
-
 
 /* make initial state null for all keys in initial state not declared in reducers */
 Object.keys(initialState).forEach((item) => {
@@ -208,19 +220,16 @@ const storeReducer = (state, action) => {
   return combinedReducers(modifiedState, action);
 };
 
-
-
-
 const store = createStore(
   storeReducer,
-  initialState,
+  initialState
 
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 store.subscribe(() => {
   saveState({
-    playerInfo: store.getState().playerInfo
+    playerInfo: store.getState().playerInfo,
   });
 });
 
