1d4785fc9a358fa6811b814c3f68cf30e3ecf291
Jakub Lasek <jakublasek41@gmail.com>
2021-09-09 23:25:29 +0200

    Add employee market design with no rwd

--- projectx/src/components/views/StockMarket/StockMarket2/StockMarket2.js
+++ projectx/src/components/views/StockMarket/StockMarket2/StockMarket2.js
@@ -3,12 +3,27 @@ import InnerWindow from "../../../layout/InnerWindow/InnerWindow";
 import OuterWindow from "../../../layout/OuterWindow/OuterWindow";
 // import styles from "./StockMarket2.module.scss";
 import Section from "../../../layout/Section/Section";
+import EMPLOYEES from "../../../../data/employes.json";
+import EmployeeElement from "./EmployeeElement/EmployeeElement";
 
-const StockMarket2 = () => (
+const StockMarket2 = ({ ...props }) => (
   <Section>
     <OuterWindow>
       <InnerWindow>
-        <h1>Employees market is still in develpoment</h1>
+        {EMPLOYEES.map((employee) => (
+          <EmployeeElement
+            key={employee.id}
+            name={employee.name}
+            perfomanceIncreased={employee.performanceIncreased}
+            productionTimeBoost={employee.productionTimeBoost}
+            experienceBoost={employee.experienceBoost}
+            productionCostBoost={employee.productionCostBoost}
+            qunatityBoost={employee.quantityBoost}
+            hireTime={employee.hireTime}
+            hireCost={employee.hireCost}
+            {...props}
+          />
+        ))}
       </InnerWindow>
     </OuterWindow>
   </Section>
