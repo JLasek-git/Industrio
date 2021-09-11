4ee73866b2509d8be9234bcd41cb677c32c61aa0
Jakub Lasek <jakublasek41@gmail.com>
2021-09-11 15:48:30 +0200

    Change of material amount fixed in dashboard view

--- projectx/src/components/views/Game/MagazinePanel/MagazinePanel.js
+++ projectx/src/components/views/Game/MagazinePanel/MagazinePanel.js
@@ -19,14 +19,14 @@ function MaterialPanel({ ...props }) {
         <span>
           Iron ore:{" "}
           <span className={styles.materialNumber}>
-            {props.playerInfo.equipment.materials.ironOre.quantity.toFixed(1)}t
+            {props.playerInfo.equipment.materials.ironOre.quantity.toFixed(0)}t
           </span>
         </span>
         <span>
           Iron ore concentrate:{" "}
           <span className={styles.materialNumber}>
             {props.playerInfo.equipment.materials.ironOreConcentrate.quantity.toFixed(
-              1
+              0
             )}
             t
           </span>
