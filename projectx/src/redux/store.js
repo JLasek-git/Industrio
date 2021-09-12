35bbc890d8976dde90fa5c7acf217b90ad56180b
Jakub Lasek <jakublasek41@gmail.com>
2021-09-12 12:46:45 +0200

    Minor change in store.js

--- projectx/src/redux/store.js
+++ projectx/src/redux/store.js
@@ -249,8 +249,8 @@ const storeReducer = (state, action) => {
 
 const store = createStore(
   storeReducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  initialState
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 store.subscribe(() => {
