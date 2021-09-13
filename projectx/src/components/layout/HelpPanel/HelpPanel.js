cd3758a41a7b795b10f6adf10bb0c81a52225709
Jakub Lasek <jakublasek41@gmail.com>
2021-09-13 20:35:33 +0200

    Balance changes

---
+++ projectx/src/components/layout/HelpPanel/HelpPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HelpPanel.module.scss";
+
+function HelpPanel() {
+  return (
+    <div className={styles.info}>
+      <p>Informacje</p>
+    </div>
+  );
+}
+
+export default HelpPanel;
